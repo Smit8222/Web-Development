db5a0434843f35c731f1790a5020c007078673a0
Smit8222 <smitkhobragade664@gmail.com>
2025-08-03 13:35:06 +0530

    .

--- Backend/EJSdir/index.js
+++ Backend/EJSdir/index.js
@@ -4,6 +4,7 @@ const path = require("path");
 
 const port = 8080;
 
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -15,8 +16,17 @@ app.get("hello", (req, res) => {
     res.send("hello");
 });
 
+app.get("/ig/:username", (req, res) => {
+    let { username } = req.params;
+    const instData = require("./data.json");
+    const data = instData[username];
+    console.log(data);
+    res.render("instagram.ejs", { data : instData[username] });
+});
+
 app.get("/rolldice", (req, res) => {
-    res.render("rolldice.ejs");
+    let diceVal = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice.ejs", {diceVal});
 });
 
 app.listen(port, () => {
